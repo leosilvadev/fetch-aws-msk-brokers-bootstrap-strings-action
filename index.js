2d57aeb56c9db96ed94a8453c3b4bda141824c83
Leonardo Silva <leosilvadev@gmail.com>
2021-06-14 10:17:15 +0200

    add logging

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const {
 const region = core.getInput('region');
 const clusterArn = core.getInput('cluster-arn');
 
+console.log(`Fetchings cluster info for region ${region}`);
+
 const client = new KafkaClient({ region: region });
 
 client.send(new ListClustersCommand({})).then(data => {
+    console.log(`Found ${data.ClusterInfoList.length} clusters, getting brokers string from the first one`);
     return data.ClusterInfoList[0].ClusterArn;
 }).then(clusterArn => {
     return client.send(new GetBootstrapBrokersCommand({ 'ClusterArn': clusterArn }));
 }).then(result => {
+    console.log(`Found given brokers string: JSON.stringify(result)`);
     core.setOutput('brokers_string', result.BootstrapBrokerString);
     core.setOutput('brokers_sasl_iam_string', result.BootstrapBrokerStringSaslIam);
     core.setOutput('brokers_sasl_scram_string', result.BootstrapBrokerStringSaslScram);
