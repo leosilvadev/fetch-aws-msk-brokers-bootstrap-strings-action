edfbd20297d18706b291169306341b6100387483
Leonardo Silva <leosilvadev@gmail.com>
2021-06-21 12:08:04 +0200

    add escape-result input

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const {
     GetBootstrapBrokersCommand
 } = require("@aws-sdk/client-kafka")
 
+const escapeResult = core.getInput('escape-result') ||  false;
 const region = core.getInput('region');
 const clusterArn = core.getInput('cluster-arn');
 
 console.log(`Fetchings cluster info for region ${region}`);
 
 const client = new KafkaClient({ region: region });
+
+const handleStringResult = value => {
+    if (value && escapeResult) {
+        return value.replace(/,/g, '\\,');
+    }
+
+    return value;
+};
 
 client.send(new ListClustersCommand({})).then(data => {
     console.log(`Found ${data.ClusterInfoList.length} clusters, getting brokers string from the first one`);
@@ -30,12 +39,12 @@ client.send(new ListClustersCommand({})).then(data => {
 
 }).then(result => {
     console.log(`Found given brokers string: ${JSON.stringify(result)}`);
-    core.setOutput('zookeeper_string', result.zookeeperConnectString);
-    core.setOutput('zookeeper_tls_string', result.zookeeperConnectStringTls);
-    core.setOutput('brokers_string', result.BootstrapBrokerString);
-    core.setOutput('brokers_sasl_iam_string', result.BootstrapBrokerStringSaslIam);
-    core.setOutput('brokers_sasl_scram_string', result.BootstrapBrokerStringSaslScram);
-    core.setOutput('brokers_sasl_tls_string', result.BootstrapBrokerStringTls);
+    core.setOutput('zookeeper_string', handleStringResult(result.zookeeperConnectString));
+    core.setOutput('zookeeper_tls_string', handleStringResult(result.zookeeperConnectStringTls));
+    core.setOutput('brokers_string', handleStringResult(result.BootstrapBrokerString));
+    core.setOutput('brokers_sasl_iam_string', handleStringResult(result.BootstrapBrokerStringSaslIam));
+    core.setOutput('brokers_sasl_scram_string', handleStringResult(result.BootstrapBrokerStringSaslScram));
+    core.setOutput('brokers_sasl_tls_string', handleStringResult(result.BootstrapBrokerStringTls));
 }).catch(error => {
     core.setFailed(error);
 });
