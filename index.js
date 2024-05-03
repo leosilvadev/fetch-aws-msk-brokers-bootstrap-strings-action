5fc63818c134d994f1df08dcd4f5ddb3f9d910c8
Philip Fieber <philip.fieber@mobimeo.com>
2024-05-03 11:37:56 +0200

    update to node 20, refactoring, modernizing

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const {
-    KafkaClient,
-    ListClustersCommand,
-    GetBootstrapBrokersCommand
+  KafkaClient,
+  ListClustersCommand,
+  GetBootstrapBrokersCommand
 } = require("@aws-sdk/client-kafka")
 
-const escapeResult = core.getInput('escape-result') ||  false;
-const region = core.getInput('region');
-const clusterArn = core.getInput('cluster-arn');
+const ESCAPE_RESULT = core.getInput('escape-result') || false;
+const REGION = core.getInput('region');
 
-console.log(`Fetchings cluster info for region ${region}`);
-
-const client = new KafkaClient({ region: region });
+console.log(`Fetchings cluster info for region ${REGION}`);
 
 const handleStringResult = value => {
-    if (value && escapeResult) {
-        return value.replace(/,/g, '\\,');
-    }
+  if (value && ESCAPE_RESULT) {
+    return value.replace(/,/g, '\\,');
+  }
 
-    return value;
+  return value;
 };
 
-client.send(new ListClustersCommand({})).then(data => {
+async function run() {
+  try {
+    const client = new KafkaClient({ region: REGION });
+    
+    const data = await client.send(new ListClustersCommand({}));
     console.log(`Found ${data.ClusterInfoList.length} clusters, getting brokers string from the first one`);
     const firstCluster = data.ClusterInfoList[0];
-    return {
-        clusterArn: firstCluster.ClusterArn,
-        zookeeperConnectString: firstCluster.ZookeeperConnectString,
-        zookeeperConnectStringTls: firstCluster.ZookeeperConnectStringTls
+    const cluster = {
+      clusterArn: firstCluster.ClusterArn,
+      zookeeperConnectString: firstCluster.ZookeeperConnectString,
+      zookeeperConnectStringTls: firstCluster.ZookeeperConnectStringTls
     };
-
-}).then(cluster => {
-    const { clusterArn } = cluster;
-    return client.send(
-        new GetBootstrapBrokersCommand({ 'ClusterArn': clusterArn })
-    ).then(result => Object.assign({}, result, cluster));
-
-}).then(result => {
+    
+    const result = await client.send(
+      new GetBootstrapBrokersCommand({ 'ClusterArn': cluster.clusterArn })
+    );
+    
     console.log(`Found given brokers string: ${JSON.stringify(result)}`);
-    core.setOutput('zookeeper_string', handleStringResult(result.zookeeperConnectString));
-    core.setOutput('zookeeper_tls_string', handleStringResult(result.zookeeperConnectStringTls));
+    core.setOutput('zookeeper_string', handleStringResult(cluster.zookeeperConnectString));
+    core.setOutput('zookeeper_tls_string', handleStringResult(cluster.zookeeperConnectStringTls));
     core.setOutput('brokers_string', handleStringResult(result.BootstrapBrokerString));
     core.setOutput('brokers_sasl_iam_string', handleStringResult(result.BootstrapBrokerStringSaslIam));
     core.setOutput('brokers_sasl_scram_string', handleStringResult(result.BootstrapBrokerStringSaslScram));
     core.setOutput('brokers_sasl_tls_string', handleStringResult(result.BootstrapBrokerStringTls));
-}).catch(error => {
+  } catch (error) {
     core.setFailed(error);
-});
+  }
+}
+
+(async() => {
+  await run();
+})();
